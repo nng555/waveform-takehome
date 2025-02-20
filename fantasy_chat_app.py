383013ff8c4de660b5a5aeb260628bdcc82961e3
Nathan Ng <nathanng@macbookpro.mynetworksettings.com>
2025-02-20 00:43:18 -0500

    set google servers

--- fantasy_chat_app.py
+++ fantasy_chat_app.py
@@ -86,14 +86,16 @@ def get_ice_servers():
     """
 
     # Ref: https://www.twilio.com/docs/stun-turn/api
+    """
     try:
         account_sid = os.environ["TWILIO_ACCOUNT_SID"]
         auth_token = os.environ["TWILIO_AUTH_TOKEN"]
     except KeyError:
-        logger.warning(
-            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
-        )
-        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+    """
+    logger.warning(
+        "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+    )
+    return [{"urls": ["stun:stun.l.google.com:19302"]}]
 
     client = Client(account_sid, auth_token)
 
